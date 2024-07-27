62d3e45e0335a7d67ecbd7e138c67c68e2cc4855
Roman Kuzmin <ghelatin@mail.ru>
2024-07-27 22:33:14 +0300

    Rename Application

---
+++ src/main/java/ru/prostostudia/homeworkone/HomeworkOneApplication.java
@@ -0,0 +1,14 @@
+package ru.prostostudia.homeworkone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkOneApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HomeworkOneApplication.class, args);
+	}
+
+}
